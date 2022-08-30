b485ad49c17f738f77cf7c778c7dfc8f24fd0002
smith <smithavarghese73@gmail.com>
2022-08-30 14:10:28 +0530

    first commit

---
+++ src/main/java/resources/Base.java
@@ -0,0 +1,29 @@
+package resources;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Base {
+	public WebDriver driver;
+	public WebDriver initializedriver() throws IOException {
+		Properties prop=new Properties();
+	
+	FileInputStream fis=new FileInputStream("C:\\Users\\smith\\eclipse-workspace\\E2EProject\\src\\main\\java\\resources\\data.properties");
+	prop.load(fis);//give knowledge of the property file
+	String browsername=prop.getProperty("browser");
+	if(browsername.equals("chrome")) //cannot use "==" while extracting value from property
+	{
+		 driver=new ChromeDriver();
+		  System.setProperty("webdriver.chrome.driver","C:\\Users\\smith\\eclipse-workspace\\E2EProject\\chromedriver.exe");
+	}
+	driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+	return driver;//so that we could use in our testcases
+	}
+
+}
